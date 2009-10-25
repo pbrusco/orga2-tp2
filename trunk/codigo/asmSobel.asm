8b0f357f2fc8a749115f5417778aec76525775e0
marianobianchi08 <marianobianchi08@2eb4cae2-b2e6-11de-8db9-a33e7b47b29e>
2009-10-25 06:49:23 +0000

    Ya comente sobel.

--- trunk/codigo/asmSobel.asm
+++ trunk/codigo/asmSobel.asm
@@ -23,13 +23,13 @@ asmSobel:
 
 	convC_push				
 
-	mov esi, src				
-	mov edi, dst				
+	mov esi, src			;cargo en esi el puntero a la imagen fuente		
+	mov edi, dst			;cargo en edi el puntero a la imagen destino	
 	mov ecx, alto
-	sub ecx, 1		;VER POR QUE ASI FUNCA!!! Y CON 2 TAMBIEN, PERO SI COMPARO CON SOBEL, CON 2 ME COMO LA ULTIMA LINEA
-	mov eax, paso
-	mov edx, ancho
-	sub edx,8
+	sub ecx, 2			;ecx = alto - 2 (porque tanto la primera como la ultima fila no las proceso)
+	mov eax, paso			;en eax dejo el tamaño del ancho de la imagen (con basura)
+	mov edx, ancho			
+	sub edx,8			;edx = ancho - 8 (lo voy a usar para iterar sobre cada fila de la imagen)
 	pxor xmm3,xmm3			;lo voy a usar como mascara para desempaquetar
 	pxor xmm6,xmm6			;aca voy a almacenar el resultado de hacer sobelX
 	pxor xmm7,xmm7			;aca voy a almacenar el resultado de hacer sobelY
@@ -37,48 +37,48 @@ asmSobel:
 
 	recorroFilas:
 
-		xor ebx, ebx				;pongo en 0 el contador de pixeles procesados
+		xor ebx, ebx				;lo voy a usar para contar los pixeles procesados
 		
 		
 		recorroColumnas:
-			pxor xmm5,xmm5			;aca voy a almacenar el resultado de hacer el sobel que me pidieron
+			pxor xmm5,xmm5			;aca voy a almacenar el resultado de aplicar el operador de sobel que me pidieron
 			cmp ebx, edx			;me fijo si estoy terminando la fila
-			jg procesoLosUltimosDeLaFila
+			jg acomodoIndices		;si estoy al final de la fila, acomodo los punteros para poder procesar los ultimos pixeles
 
 		veoSiHagoSobelX:
-			cmp dword xorder, 1			;veo si tengo que hacer sobelX
+			cmp dword xorder, 1		;veo si tengo que hacer sobelX
 			jne hagoSobelY
 		hagoSobelX:
-			jmp sobelX				;deja en xmm6 el resultado de hacer sobelX
+			jmp sobelX			;deja en xmm6 el resultado de hacer sobelX
 		veoSiHagoSobelY:
 			cmp dword yorder, 1
 			jne sigoColumnas
 		hagoSobelY:
-			jmp sobelY				;deja el resultado en xmm7
+			jmp sobelY			;deja en xmm7 el resultado de hacer sobelY
 
 		sigoColumnas:
-			paddusb xmm5, xmm6
-			paddusb xmm5, xmm7
-			movq [edi+eax+1],xmm5
+			paddusb xmm5, xmm6		;sumo a xmm5 el resultado de sobelX (sumo 0 en caso de no tener que hacer sobelX) 
+			paddusb xmm5, xmm7		;sumo a xmm5 el resultado de sobelY (sumo 0 en caso de no tener que hacer sobelY) 
+			movq [edi+eax+1],xmm5		;guardo el resultado en las posiciones que corresponden
 
 			cmp ebx, edx
-			jg sigoFilas			;si estoy procesando los ultimos, acomodo indices para saltar de fila
+			jg sigoFilas			;si acabo de procesar los ultimos pixeles de la fila, acomodo indices para cambiar de fila
 
-			lea esi,[esi+6]
-			lea edi,[edi+6]
-			add ebx,6
-			jmp recorroColumnas
+			lea esi,[esi+6]			;hago que esi apunte a los proximos 6 pixeles a procesar
+			lea edi,[edi+6]			;acomodo edi para guardar los proximos resultados
+			add ebx,6			;agrego 6 al contador de pixeles pixeles procesados
+			jmp recorroColumnas		;sigo recorriendo la misma fila
 
 	sigoFilas:
 		
-		sub esi, edx ;hacemos esi -( ancho - 8) 	
-		add esi, eax ;esi = esi + paso
+		sub esi, edx 		;hacemos esi -( ancho - 8)
+		add esi, eax 		;esi = esi + paso (esi apunta al comienzo de la siguiente fila)
 
-		sub edi, edx ;hacemos edi -( ancho - 8) 	
-		add edi, eax ;edi = edi + paso
-		dec ecx
-		cmp ecx, 0
-		jne recorroFilas
+		sub edi, edx 		;hacemos edi -( ancho - 8) 	
+		add edi, eax		;edi = edi + paso (edi apunta al comienzo de la siguiente fila)
+		dec ecx			;decremento el contador de filas
+		cmp ecx, 0		;veo si termine de procesar la imagen
+		jne recorroFilas	;si no termine, paso a procesar la fila que sigue
 
 
 
@@ -92,13 +92,13 @@ asmSobel:
 
 
 
-procesoLosUltimosDeLaFila:
+acomodoIndices:
 
 	add esi,edx
-	sub esi,ebx
+	sub esi,ebx			;dejo a esi apuntando a los ultimos 8 pixeles de la fila actual
 	add edi,edx
-	sub edi,ebx
-	jmp veoSiHagoSobelX
+	sub edi,ebx			;dejo a edi apuntando a los ultimos 8 pixeles de la fila actual
+	jmp veoSiHagoSobelX		;una vez acomodado los indices, proceso los ultimos 6 pixeles de la fila actual
 
 
 
@@ -110,30 +110,36 @@ procesoLosUltimosDeLaFila:
 sobelX:
 		;esi apunta a la imagen fuente, al pixel donde debo empezar a cargar los datos a procesar
 		;no pisar xmm3, xmm5, xmm7
-		movq xmm0,[esi]
-		movq xmm1,[esi+eax]
-		movq xmm2,[esi+eax*2]
 
-		punpcklbw xmm0,xmm3
-		punpcklbw xmm1,xmm3
-		punpcklbw xmm2,xmm3
+		movq xmm0,[esi]			;cargo en la parte menos significativa de xmm0, la primer tira de pixeles
+		movq xmm1,[esi+eax]		;cargo en la parte menos significativa de xmm1, la segunda tira de pixeles
+		movq xmm2,[esi+eax*2]		;cargo en la parte menos significativa de xmm2, la tercer tira de pixeles
+
+		punpcklbw xmm0,xmm3		;desempaqueto de byte a word la parte menos significativa de xmm0
+		punpcklbw xmm1,xmm3		;desempaqueto de byte a word la parte menos significativa de xmm1
+		punpcklbw xmm2,xmm3		;desempaqueto de byte a word la parte menos significativa de xmm2
+
 
 		paddusw xmm0,xmm1
 		paddusw xmm0,xmm1
-		paddusw xmm0,xmm2
+		paddusw xmm0,xmm2		;xmm0 = xmm0 + 2* xmm1 + xmm2
 
-		movdqu xmm1,xmm0
+		movdqu xmm1,xmm0		;copio xmm0
 	
-		psrldq xmm1,4
+		psrldq xmm1,4			;shifteo 2 words a derecha xmm1
 	
-		psubusw xmm1,xmm0
+		psubusw xmm1,xmm0		;luego de esta resta, me quedan en los 6 bytes menos significativos 
+						;de xmm1 el resultado de sobelX
 
-		PACKUSWB xmm1,xmm1
-		PSLLQ xmm1,16		
-		PSRLQ xmm1,16
-		movdqu xmm6, xmm1
+		PACKUSWB xmm1,xmm1		;empaqueto xmm1 para poder guardar el resultado
+		PSLLQ xmm1,16
+		PSRLQ xmm1,16			;shifteo 2 bytes a izquierda y derecha a xmm1 para dejar 0's en los 2 bytes
+						;mas significativos de ambas qwords del registro, para guardar los 8 bytes
+						;directamente en la imagen destino, sin preocuparme de guardar basura
+
+		movdqu xmm6, xmm1		;dejo el resultado de hacer sobelX en xmm6
 		
-		jmp veoSiHagoSobelY
+		jmp veoSiHagoSobelY		;me fijo si tengo que usar el operador sobelY
 
 
 
@@ -143,36 +149,42 @@ sobelX:
 sobelY:
 		;esi apunta a la imagen fuente, al pixel donde debo empezar a cargar los datos a procesar
 		;no pisar xmm3, xmm5, xmm6
-		movq xmm0,[esi]
-		movq xmm1,[esi+eax*2]
+
+		movq xmm0,[esi]			;cargo en la parte menos significativa de xmm0, la primer tira de pixeles
+		movq xmm1,[esi+eax*2]		;cargo en la parte menos significativa de xmm1, la tercer tira de pixeles
 		
 
-		punpcklbw xmm0,xmm3
-		punpcklbw xmm1,xmm3
+		punpcklbw xmm0,xmm3		;desempaqueto de byte a word la parte menos significativa de xmm0
+		punpcklbw xmm1,xmm3		;desempaqueto de byte a word la parte menos significativa de xmm1
 		
-		movdqu xmm2, xmm0
-		movdqu xmm4, xmm0
-		psrldq xmm2, 2
-		psrldq xmm4, 4
+		movdqu xmm2, xmm0		;copio xmm0 en xmm2
+		movdqu xmm4, xmm0		;copio xmm0 en xmm4
+		psrldq xmm2, 2			;shifteo xmm2 1 word a derecha
+		psrldq xmm4, 4			;shifteo xmm4 2 words a derecha
+		paddusw xmm0,xmm2		
 		paddusw xmm0,xmm2
-		paddusw xmm0,xmm2
-		paddusw xmm0,xmm4
+		paddusw xmm0,xmm4		;dejo en las 6 words menos significativas de xmm0 el resultado de aplicar
+						;la primer fila de la mascara de sobelY
 
-		movdqu xmm2, xmm1
-		movdqu xmm4, xmm1
-		psrldq xmm2, 2
-		psrldq xmm4, 4
+		movdqu xmm2, xmm1		;copio xmm1 en xmm2
+		movdqu xmm4, xmm1		;copio xmm1 en xmm4
+		psrldq xmm2, 2			;shifteo xmm2 1 word a derecha
+		psrldq xmm4, 4			;shifteo xmm2 2 words a derecha
 		paddusw xmm1,xmm2
 		paddusw xmm1,xmm2
-		paddusw xmm1,xmm4
+		paddusw xmm1,xmm4		;dejo en las 6 words menos significativas de xmm1 el resultado de aplicar
+						;la tercer fila de la mascara de sobelY
 
 
-		psubusw xmm1, xmm0 
+		psubusw xmm1, xmm0 		;hago la suma final, es decir, aplico completamente la mascara de sobelY
 
 
-		PACKUSWB xmm1,xmm1
+		PACKUSWB xmm1,xmm1		;empaqueto xmm1 para poder guardar el resultado
 		PSLLQ xmm1,16		
-		PSRLQ xmm1,16
-		movdqu xmm7, xmm1
+		PSRLQ xmm1,16			;shifteo 2 bytes a izquierda y derecha a xmm1 para dejar 0's en los 2 bytes
+						;mas significativos de ambas qwords del registro, para guardar los 8 bytes
+						;directamente en la imagen destino, sin preocuparme de guardar basura
+
+		movdqu xmm7, xmm1		;dejo el resultado de hacer sobelY en xmm7
 		
-		jmp sigoColumnas
+		jmp sigoColumnas		;paso a acomodar los indices para la proxima iteracion
