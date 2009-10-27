57f8779e98480692312841fffc588c2a203e9635
cdipietro11 <cdipietro11@2eb4cae2-b2e6-11de-8db9-a33e7b47b29e>
2009-10-27 05:04:10 +0000

    ANDA! Ccuando les cuente pq no andaba no lo van a poder creer

--- codigo/asmFreiChen.asm
+++ codigo/asmFreiChen.asm
@@ -15,7 +15,6 @@ section .data
 	%define var_local_1					[ebp-4]
 
 	
-
 section .text
 
 asmFreiChen:
@@ -30,7 +29,7 @@ asmFreiChen:
 	mov eax, HEIGHT				; eax = HEIGHT
 	sub eax, 2				; eax = HEIGHT-2
 	mov ecx, WIDTH				; ecx = WIDTH
-	mul ecx
+	mul ecx					; eax = (HEIGHT-2) * WIDTH
 	mov ecx, eax				; ecx = (HEIGHT-2) * WIDTH
 	shr ecx, 1				; ecx = (HEIGHT-2) * WIDTH/2 -- voy a procesar 2 píxeles por vez
 	mov eax, WIDTH				; eax = WIDTH
@@ -41,18 +40,19 @@ asmFreiChen:
 	mov esi, DIR_SRC			; esi = puntero a la matriz fuente
 	mov edi, DIR_DST			; edi = puntero a la matriz destino
 	
-		
-	; calculo la raiz cuadrada de 2 con la FPU, la guardo en var_local_1 y luego la cargo en XMM4
 
-	finit					; inicializo la FPU
-	fld1					; st0 = 1
-	fadd st0				; st0 = 2
-	fsqrt					; st0 = 2^½	
-	fstp dword var_local_1			; var_local_1 = 2^½
+	; cargo la variable local con el valor entero 2
+	
+	mov dword var_local_1, 2	; var_local_1 = 2
 
-	movss xmm4, var_local_1		; xmm4 =     |     |     | 2^½		
-	pshufd xmm4, xmm4, 00000000b	; xmm4 = 2^½ | 2^½ | 2^½ | 2^½
 
+	; cargo XMM4 4 veces con el entero 2, convierto cada paquete en un flotante y le calculo la raiz cuadrada
+
+	movss xmm4, var_local_1		; xmm4 = - | - | - | 2		(32 bits inc c/u)
+	pshufd xmm4, xmm4, 00000000b	; xmm4 = 2 | 2 | 2 | 2		(32 bits inc c/u)
+	cvtdq2ps xmm4, xmm4		; xmm4 = 2 | 2 | 2 | 2		(32 bits float c/u)
+	sqrtps xmm4, xmm4		; xmm4 = 2^½ | 2^½ | 2^½ | 2^½	(32 bits float c/u)
+	
 	
 	; a continuación, en el ciclo que sigue recorro la matriz aplicando la máscara de a 2 píxeles por vez
 	
@@ -61,7 +61,7 @@ asmFreiChen:
 ; respecto a su posición en la matriz, es decir, en la matriz la numeración de los píxeles crece de izquierda a 
 ; derecha, mientras que en los registros xmm la numeración de los píxeles crece de derecha a izquierda
 
-; ACLARACIÓN 2: Los pixeles de la matriz se indexan como pij, donde i = fila (1 a HEIGHT) y j = columna (0 a WHIDTH)
+; ACLARACIÓN 2: Los pixeles de la matriz se indexan como pij, donde i = fila (1 a HEIGHT-2) y j = columna (0 a WHIDTH)
 ; --------------------------------------------------------------------------------------------------------------------
 
 	.ciclo: 
@@ -70,24 +70,25 @@ asmFreiChen:
 		; y cargo los registros XMM5 y XMM6 con el contenido de XMM4
 		
 		pxor xmm0, xmm0			; xmm0 = 0 
-		movd xmm1, [esi]		; xmm1 = p13|p12|p11|p10  (p* = 8 bits int)
-		movd xmm2, [esi+eax]		; xmm2 = p23|p22|p21|p20  (p* = 8 bits int)
-		movd xmm3, [esi+eax*2]		; xmm3 = p33|p32|p31|p30  (p* = 8 bits int)
+		
+		movd xmm1, [esi]		; xmm1 =  0|0|0|0|0|0|0|0|0|0|0|0|p13|p12|p11|p10  (p* = 8 bits int)
+		movd xmm2, [esi+eax]		; xmm2 =  0|0|0|0|0|0|0|0|0|0|0|0|p23|p22|p21|p20  (p* = 8 bits int)
+		movd xmm3, [esi+eax*2]		; xmm3 =  0|0|0|0|0|0|0|0|0|0|0|0|p33|p32|p31|p30  (p* = 8 bits int)
 
-		movdqu xmm5, xmm4		; xmm5 = 2^½ | 2^½ | 2^½ | 2^½
-		movdqu xmm6, xmm4		; xmm6 = 2^½ | 2^½ | 2^½ | 2^½
+		movdqu xmm5, xmm4		; xmm5 = 2^½ | 2^½ | 2^½ | 2^½			   (32 bits float c/u)
+		movdqu xmm6, xmm4		; xmm6 = 2^½ | 2^½ | 2^½ | 2^½			   (32 bits float c/u)
 
 
 		; desempaqueto los 4 píxeles de cada una de las 3 filas de bytes a word, y luego de word a doubleword
 		
-		punpcklbw xmm1, xmm0		; xmm1 = p13|p12|p11|p10  (p* = 16 bits int)
-		punpcklwd xmm1, xmm0		; xmm1 = p13|p12|p11|p10  (p* = 32 bits int)
+		punpcklbw xmm1, xmm0		; xmm1 = 0|0|0|0|p13|p12|p11|p10  (p* = 16 bits int)
+		punpcklwd xmm1, xmm0		; xmm1 = p13|p12|p11|p10	  (p* = 32 bits int)
 
-		punpcklbw xmm2, xmm0		; xmm2 = p23|p22|p21|p20  (p* = 16 bits int)
-		punpcklwd xmm2, xmm0		; xmm2 = p23|p22|p21|p20  (p* = 32 bits int)
+		punpcklbw xmm2, xmm0		; xmm2 = 0|0|0|0|p23|p22|p21|p20  (p* = 16 bits int)
+		punpcklwd xmm2, xmm0		; xmm2 = p23|p22|p21|p20	  (p* = 32 bits int)
 
-		punpcklbw xmm3, xmm0		; xmm3 = p33|p32|p31|p30  (p* = 16 bits int)
-		punpcklwd xmm3, xmm0		; xmm3 = p33|p32|p31|p30  (p* = 32 bits int)
+		punpcklbw xmm3, xmm0		; xmm3 = 0|0|0|0|p33|p32|p31|p30  (p* = 16 bits int)
+		punpcklwd xmm3, xmm0		; xmm3 = p33|p32|p31|p30	  (p* = 32 bits int)
 
 
 		; convierto los valores de los 4 píxeles de cada una de las 3 filas de enteros a punto flotante sp
@@ -99,50 +100,56 @@ asmFreiChen:
 
 		; aplico la máscara de Frei-Chen en X para a los píxeles p22 y p21
 
-		mulps xmm2, xmm4		; xmm2 =  p23*2^½ | p22*2^½ | p21*2^½ | p20*2^½
 		movdqu xmm7, xmm3		; xmm6 = p33|p32|p31|p30 
+		mulps xmm2, xmm4		; xmm2 =  p23*2^½ | p22*2^½ | p21*2^½ | p20*2^½
 
-		addps xmm7, xmm1		; xmm7 = p33+p13 | p32+p12 | p31+p11 | p30+p10
+		addps xmm7, xmm1		; xmm7 = p33+p13 | p32+p12 | p31+p11 | p30+p10		
 		addps xmm7, xmm2		; xmm7 = p33+p13+p23*2^½|p32+p12+p22*2^½|p31+p11+p21*2^½|p30+p10+p20*2^½
+
 		pshufd xmm0, xmm7, 11111110b	; xmm0 = p33+p13+p23*2^½|p33+p13+p23*2^½|p33+p13+p23*2^½|p32+p12+p22*2^½
 
 		subps xmm0, xmm7		; xmm0 =  - | - | Frey-ChenX(p22) | Frey-ChenX(p21)
-
-		pxor mm0, mm0			; mm0 = 0
-		cvtps2pi mm0, xmm0		; mm0 = Frey-ChenX(p22) | Frey-ChenX(p21)  (32 bits int c/u)
 
 
 		; aplico la máscara de Frei-Chen en Y para a los píxeles p22 y p21
 
 		mulps xmm5, xmm1		; xmm5 =  p13*2^½ | p12*2^½ | p11*2^½ | p10*2^½		
 		pshufd xmm5, xmm5, 11111001b	; xmm5 =  p13*2^½ | p13*2^½ | p13*2^½ | p12*2^½ 
-		
+	
 		mulps xmm6, xmm3		; xmm6 =  p33*2^½ | p32*2^½ | p31*2^½ | p30*2^½		
 		pshufd xmm6, xmm6, 11111001b	; xmm6 =  p33*2^ ½ | p33*2^½ | p33*2^½ | p32*2^½ 
-		
+
+		subps xmm6, xmm5		; xmm6 =  - | - | (p33-p13)*2^½ | (p32-p12)*2^½ 
+
 		subps xmm1, xmm3		; xmm1 =  p33-p13| p32-p12| p31-p11| p30-p10
 		pshufd xmm3, xmm1, 11111110b	; xmm3 =  p33-p13| p33-p13| p33-p13| p32-p12
-
 		addps xmm1, xmm3		; xmm1 =  - | - | p31-p11+p33-p13| p30-p10+p32-p12
-		addps xmm1, xmm6		; xmm1 =  - | - | p31-p11+p33-p13+p33*2^½ | p30-p10+p32-p12+p32*2^½
-		subps xmm1, xmm5		; xmm1 =  - | - | Frey-ChenY(p22) | Frey-ChenY(p21)
+
+		addps xmm1, xmm6		; xmm1 =  - | - | Frey-ChenY(p22) | Frey-ChenY(p21)		
+
+
+		; saturo los píxeles procesados en la derivada X e Y guardados en XMM0 y XMM1 respectivamente
+				
+		pxor xmm2, xmm2			; xmm2 = 0
+		cmpps xmm2, xmm0, 2		; xmm2 tiene FFFFh en aquellos paquetes que son negativos
+		pand xmm0, xmm2			; los paquetes negativos de xmm0 son convertidos en 0
+
+		pxor xmm2, xmm2			; xmm2 = 0
+		cmpps xmm2, xmm1, 2		; xmm2 tiene FFFFh en aquellos paquetes que son negativos
+		pand xmm1, xmm2			; los paquetes negativos de xmm1 son convertidos en 0
+
+
+		; convierto los valores de los píxeles de punto flotante a enteros nuevamente
 		
+		pxor mm0, mm0			; mm0 = 0
+		cvtps2pi mm0, xmm0		; mm0 = Frey-ChenX(p22) | Frey-ChenX(p21)  (32 bits int c/u)
+				
 		pxor mm1, mm1			; mm1 = 0
 		cvtps2pi mm1, xmm1		; mm1 = Frey-ChenY(p22) | Frey-ChenY(p21)  (32 bits int c/u)
 
 
-		; saturo los posibles valores negativos de los píxeles a 0
-		
-		pxor mm2, mm2			; mm2 = 0
-		pcmpgtd mm2, mm0
-		pand mm0, mm2
-
-		pxor mm2, mm2			; mm2 = 0
-		pcmpgtd mm2, mm1
-		pand mm1, mm2
-
-		; empaqueto los pixeles procesado en X e Y en los registros XMM0 y XMM1 a words  primero, luego a bytes 
-		; y finalmente realizo la suma empaquetada y con saturación de ambos
+		; empaqueto los pixeles procesado en la derivada X e Y y guardados en MM0 y MM1 respectivamente a
+		; words  primero, luego a bytes
 	
 		packssdw mm0, mm0		; mm0 = - | - | Frey-ChenX(p22) | Frey-ChenX(p21)  (16 bits int c/u)
 		packsswb mm0, mm0		; mm0 = - | - | - | - | Frey-ChenX(p22) | Frey-ChenX(p21)(8bits int c/u)
@@ -150,8 +157,13 @@ asmFreiChen:
 		packssdw mm1, mm1		; mm1 = - | - | Frey-ChenY(p22) | Frey-ChenY(p21)  (16 bits int c/u)
 		packsswb mm1, mm1		; mm1 = - | - | - | - | Frey-ChenY(p22) | Frey-ChenY(p21)(8bits int c/u)
 
+
+		;finalmente realizo la suma empaquetada y con saturación de ambos para obtener los valores de los
+		; píxeles en la derivada XY
+		
 		paddsb mm0, mm1			; mm0 = - | - | - | - | Frey-ChenXY(p22) | Frey-ChenXY(p21)
 		
+
 		; guardo los píxeles procesados en la matriz destino y avanzo los punteros para continuar el procesando
 		
 		movd [edi+eax+1], mm0		; guardo los 2 píxeles procesados en la matriz destino
